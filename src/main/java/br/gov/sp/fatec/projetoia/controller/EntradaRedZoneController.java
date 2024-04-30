850ade726c4b2737eb87ddee9053b889a153501a
fNetoCardoso <francisco14042000@gmail.com>
2024-04-29 22:39:08 -0300

    feat: count entry people by redzone
    counting people by entry redzone

--- src/main/java/br/gov/sp/fatec/projetoia/controller/EntradaRedZoneController.java
+++ src/main/java/br/gov/sp/fatec/projetoia/controller/EntradaRedZoneController.java
@@ -2,8 +2,10 @@ package br.gov.sp.fatec.projetoia.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import br.gov.sp.fatec.projetoia.entity.EntradaRedZoneEntity;
+import br.gov.sp.fatec.projetoia.responses.PeopleCountResponse;
 import br.gov.sp.fatec.projetoia.service.EntradaRedZoneService;
 
 import java.time.LocalDate;
@@ -57,6 +59,14 @@ public class EntradaRedZoneController {
             @RequestParam("startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
             @RequestParam("endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
         return serv.countPeopleEnteredRedZoneByDate(startDate, endDate);
+    }
+
+    @GetMapping(value = "/people-by-redzone-filter")
+    public ResponseEntity<PeopleCountResponse> countPeopleInRedZoneByFilter(
+            @RequestParam Long redZoneId) {
+        Long numberOfPeople = serv.countPeopleInRedZone(redZoneId);
+        PeopleCountResponse countResponse = new PeopleCountResponse(numberOfPeople.intValue());
+        return ResponseEntity.ok(countResponse);
     }
 
 }
