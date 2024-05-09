2c02d79ab7f9f69f5a9ba1e804459f48a8fa2eee
nlemuel <lemuelayres@gmail.com>
2024-05-08 22:29:56 -0300

    feat: crud area

---
+++ src/main/java/br/gov/sp/fatec/projetoia/entity/AreaEntity.java
@@ -0,0 +1,47 @@
+package br.gov.sp.fatec.projetoia.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "area")
+public class AreaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "descricao")
+    private String descricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
---
+++ src/main/java/br/gov/sp/fatec/projetoia/repository/AreaRepository.java
@@ -0,0 +1,8 @@
+package br.gov.sp.fatec.projetoia.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.gov.sp.fatec.projetoia.entity.AreaEntity;
+
+public interface AreaRepository extends JpaRepository<AreaEntity, Long> {
+}
