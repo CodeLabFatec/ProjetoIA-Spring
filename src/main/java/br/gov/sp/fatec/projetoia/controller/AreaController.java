d369d56c8af059c8806d38508a76963db8cc22b4
fNetoCardoso <francisco14042000@gmail.com>
2024-05-13 15:24:35 -0300

    feat: area in redzone

--- src/main/java/br/gov/sp/fatec/projetoia/controller/AreaController.java
+++ src/main/java/br/gov/sp/fatec/projetoia/controller/AreaController.java
@@ -1,13 +1,10 @@
 package br.gov.sp.fatec.projetoia.controller;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -25,46 +22,23 @@ import br.gov.sp.fatec.projetoia.service.AreaService;
 public class AreaController {
 
     @Autowired
-    private AreaService serv;
-
-    @GetMapping
-    public List<AreaEntity> getAll() {
-        return serv.getAll();
-    }
+    private AreaService areaService;
 
     @PostMapping
-public ResponseEntity<AreaEntity> insert(@RequestBody AreaEntity data) {
-    AreaEntity createdEntity = serv.insert(data);
-    return ResponseEntity.status(HttpStatus.CREATED).body(createdEntity);
-}
-
-
-    @GetMapping(value = "/{id}")
-    public Optional<AreaEntity> getById(@PathVariable("id") Long id) {
-        return serv.getById(id);
-    }
-
-    @DeleteMapping(value = "/{id}")
-    public ResponseEntity<Void> deleteById(@PathVariable("id") Long id) {
-        Optional<AreaEntity> data = serv.getById(id);
-        if (data.isEmpty()) {
-            return ResponseEntity.notFound().build(); 
-        }
-
-        serv.delete(data.get());
-
-        return ResponseEntity.noContent().build(); 
+    public ResponseEntity<AreaEntity> insert(@RequestBody AreaEntity area) {
+        AreaEntity createdArea = areaService.insert(area);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdArea);
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<Void> update(@PathVariable("id") Long id, @RequestBody AreaEntity updatedEntity) {
-        Optional<AreaEntity> existingEntity = serv.getById(id);
-        if (existingEntity.isEmpty()) {
+    public ResponseEntity<AreaEntity> update(@PathVariable("id") Long id, @RequestBody AreaEntity updatedArea) {
+        Optional<AreaEntity> existingArea = areaService.getById(id);
+        if (existingArea.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
-        updatedEntity.setId(id);
-        serv.update(id, updatedEntity);
-        return ResponseEntity.noContent().build(); 
+        updatedArea.setId(id);
+        AreaEntity updated = areaService.update(id, updatedArea);
+        return ResponseEntity.ok(updated);
     }
-    
+
 }
