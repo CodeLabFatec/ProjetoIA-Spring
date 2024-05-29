970ce52fbfc960df35bf4728687f2e2dc97fc909
fNetoCardoso <francisco14042000@gmail.com>
2024-05-29 15:17:40 -0300

    feat: user entity and paper entity created

---
+++ src/main/java/br/gov/sp/fatec/projetoia/entity/UserEntity.java
@@ -0,0 +1,75 @@
+package br.gov.sp.fatec.projetoia.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+
+@Entity
+@Table(name = "usuario")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "senha")
+    private String senha;
+
+    @OneToOne
+    @JoinColumn(name = "id_papel")
+    private PaperEntity papel;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public PaperEntity getPapel() {
+        return papel;
+    }
+
+    public void setPapel(PaperEntity papel) {
+        this.papel = papel;
+    }
+
+}
+
